9f815ad63ff84dadc2b2cbe4f392c8b201d8333c
kuicongshao <kuicong.shao@gmail.com>
2026-06-10 09:36:29 +0800

    Fix CORS settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,12 +10,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://tianfuxue.pages.dev",
-        "https://ecb4e54a.tianfuxue.pages.dev",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
